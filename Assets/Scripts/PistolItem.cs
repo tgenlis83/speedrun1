c939bb6fd14ef93093c948899dfe3bdafd412117
OrdinaryDev83 <bryckout@gmail.com>
2026-01-06 16:35:47 +0100

    feat: basic test items like pistol and shovel

--- Assets/Scripts/PistolItem.cs
+++ Assets/Scripts/PistolItem.cs
@@ -1,16 +1,18 @@
-// ShovelItem.cs
+// PistolItem.cs
 using UnityEngine;
 
 [DisallowMultipleComponent]
-public sealed class ShovelItem : AbstractCarryItem
+public sealed class PistolItem : AbstractCarryItem
 {
     protected override void OnEquipAnim(EntityAnimation anim)
     {
-
+        anim.SetGunState(true);
     }
 
     protected override void OnUnequipAnim(EntityAnimation anim)
     {
-
+        anim.SetGunState(false);
     }
+
+    // Future pistol logic (Shoot, Reload, etc.)
 }
